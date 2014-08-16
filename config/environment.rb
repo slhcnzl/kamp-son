c7e555dd72345c74543d19e9df50a7350961cecd
msdundar <msdundars@gmail.com>
2014-08-16 14:01:15 +0300

    Environment dosyasına GMail SMTP ayarları tanımlandı.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.gmail.com',
+  :domain         => 'mail.google.com',
+  :port           => 587,
+  :user_name      => 'noreply@uzem.omu.edu.tr',
+  :password       => 'uzem12345',
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
