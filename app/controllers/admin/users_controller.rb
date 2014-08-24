88154565bf7bcbc99e0e8e06ad155c8525ede911
msdundar <msdundars@gmail.com>
2014-08-25 00:31:08 +0300

    Birini admin yap

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -27,7 +27,7 @@ class Admin::UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
+    params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation, :admin)
   end
 
   def ensure_admin
